1a28b54c9049954b831aaa30f9278ee797497ca9
Justin Long <107122532+drivemycode@users.noreply.github.com>
2024-10-31 03:46:51 +0800

    Delete 4000/code directory

--- 4000/code/midi_musical_instrument_controller/play_midi_file.py
+++
@@ -1,7 +0,0 @@
-import pygame, time
-pygame.mixer.init()
-pygame.mixer.music.load('file.midi')
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-    print('Still playing :)')
-    time.sleep(1)
